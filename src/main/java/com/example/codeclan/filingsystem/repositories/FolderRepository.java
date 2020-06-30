6838e3b328362c7e0dd337138d75d3ae8127f7ef
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-06-30 16:27:58 +0100

    repositories set up for file, folder and user

--- src/main/java/com/example/codeclan/filingsystem/repositories/FolderRepository.java
+++ src/main/java/com/example/codeclan/filingsystem/repositories/FolderRepository.java
@@ -1,4 +1,12 @@
 package com.example.codeclan.filingsystem.repositories;
 
-public interface FolderRepository {
-}
+import com.example.codeclan.filingsystem.models.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+    @Repository
+    public interface FolderRepository extends JpaRepository<Folder, Long>{
+
+    }
+
+
